488d858d390fd91eeff3a2dace6fde0867bbe5b8
AaqilBaig <aaqiladilbaig@gmail.com>
2023-11-27 20:49:51 +0530

    integrating with frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
